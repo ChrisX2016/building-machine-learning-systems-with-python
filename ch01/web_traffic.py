b21101c685459faef8e8203780766dba13913429
Chris <xuxiaxin2015@163.com>
2016-04-24 00:51:38 -0400

    add ch01

--- ch01/web_traffic.py
+++ ch01/web_traffic.py
@@ -14,3 +14,20 @@ plt.xticks([w*7*24 for w in range(10)],['week %i'%w for w in range(10)])
 plt.autoscale(tight=True)
 plt.grid()
 plt.show()
+
+# MSE error function
+def error(f, x, y):
+    return sp.sum((f(x)-y)**2)
+
+fp1, residuals, rank, sv, rcond = sp.polyfit(x, y, 1, full=True)
+f1 = sp.poly1d(fp1)
+print(error(f1, x, y))
+fx = sp.linspace(0, x[-1], 1000)
+plt.plot(fx, f1(fx), linewidth=4)
+plt.legend(["d=%i" % f1.order], loc="upper left")
+
+f2p = sp.polyfit(x, y, 2)
+f2 = sp.poly1d(f2p)
+print(error(f2, x, y))
+
+# inflection = 3.5*7*24
